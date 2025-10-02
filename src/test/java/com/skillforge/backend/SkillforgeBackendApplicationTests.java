fe04dac285b9a606cee576b61cda2f300c50426d
Spirale Cookie <spiralecookie@gmail.com>
2025-10-03 00:27:50 +0200

    Initial commit

---
+++ src/test/java/com/skillforge/backend/SkillforgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillforge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillforgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
